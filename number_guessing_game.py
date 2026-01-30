6d1a45dd79acf698e9d69988a9ec69a0fae47e75
mbelov725 <mark.belov725@gmail.com>
2026-01-29 21:31:12 -0500

    Added attempt tracking and hints

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -2,16 +2,29 @@ import random
 
 random_number = random.randint(1, 100)
 
+attempts = 0
+
 while True:
     try:
         guess = int(input("Guess the number between 1 and 100: "))
+
+        attempts += 1
+
+        difference = abs(guess - random_number)
         
         if guess > random_number:
             print("Too high!")
         elif guess < random_number:
             print("Too low!")
         else:
-            print("Congratulations! You guessed the number.")
+            print(f"Congratulations! You guessed the number in {attempts} attemps.")
             break
+
+        if difference <= 5:
+            print("Very hot")
+        elif 5 < difference <= 10:
+            print("Warm")
+        else:
+            print("Cold")
     except ValueError:
         print("Please enter a valid number")
