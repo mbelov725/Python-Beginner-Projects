b630180c97b1013058df8a5b8207be847e48d560
mbelov725 <mark.belov725@gmail.com>
2026-01-29 22:09:46 -0500

    Added levels and limited number of attmepts

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,14 +1,46 @@
 import random
 
-random_number = random.randint(1, 100)
+upper_limit = 0
+max_attempts = 0
 
-attempts = 0
+while True:
+    level = input("Chose your level (easy, medium, hard): ")
+
+    if level.lower() == "easy":
+        upper_limit = 50
+        max_attempts = 10
+
+        break
+    elif level.lower() == "medium":
+        upper_limit = 100
+        max_attempts = 7
+
+        break
+    elif level.lower() == "hard":
+        upper_limit = 500
+        max_attempts = 8
+
+        break
+
+random_number = random.randint(1, upper_limit)
+number_of_attempts = 0
 
 while True:
     try:
-        guess = int(input("Guess the number between 1 and 100: "))
+        if number_of_attempts == max_attempts:
+            print(f"Sorry, you ran out of attempts. The correct number was {random_number}")
 
-        attempts += 1
+            break
+
+        print(f"You have {max_attempts - number_of_attempts} attempts.")
+        guess = int(input(f"Guess the number between 1 and {upper_limit}: "))
+
+        if guess < 1 or guess > upper_limit:
+            print(f"Please enter a number between 1 and {upper_limit}")
+
+            continue
+
+        number_of_attempts += 1
 
         difference = abs(guess - random_number)
         
@@ -17,7 +49,7 @@ while True:
         elif guess < random_number:
             print("Too low!")
         else:
-            print(f"Congratulations! You guessed the number in {attempts} attemps.")
+            print(f"Congratulations! You guessed the number in {number_of_attempts} attempts.")
             break
 
         if difference <= 5:
