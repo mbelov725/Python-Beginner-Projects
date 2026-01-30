0df12486bae4833711ec52a51c3463da88491af5
mbelov725 <mark.belov725@gmail.com>
2026-01-30 15:46:02 -0500

    Added score

--- rock_paper_scissors_game.py
+++ rock_paper_scissors_game.py
@@ -3,6 +3,9 @@ import random
 emojis = {"r": "🪨", "p": "📄", "s": "✂️"}
 choices = ("r", "p", "s")
 
+user_score = 0
+computer_score = 0
+
 while True:
     user_choice = input("Rock, paper, or scissors? (r/p/s): ").lower()
     if user_choice not in choices:
@@ -21,10 +24,15 @@ while True:
         (user_choice == "p" and computer_choice == "r") or
         (user_choice == "s" and computer_choice == "p")):
         print("You win!")
+        user_score += 1
     else:
         print("You lose")
+        computer_score += 1
+
+    print(f"Score: You {user_score} - {computer_score} Computer")
 
     should_continue = input("Continue? (y/n): ").lower()
 
     if should_continue == "n":
+        print("Thank you for playing!")
         break
